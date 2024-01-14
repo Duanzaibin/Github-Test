ea1c3803bd8195a78f895d523ba4561029a44690
Duanzaibin <599941705@qq.com>
2024-01-14 15:00:19 +0800

    修改

--- src/Main/Test1.java
+++ src/Main/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("段载斌HandSome Boy！！");
         System.out.println("啦啦啦！！！");
+        System.out.println("ddd");
     }
 }
